939c460a788c76953916771479abced5e4cc7c0a
Pavel Solovev <pashius2010@gmail.com>
2021-02-13 20:04:02 +0400

    work with path

--- DotNetFilesAndDirectories/Program.cs
+++ DotNetFilesAndDirectories/Program.cs
@@ -8,6 +8,30 @@ namespace DotNetFilesAndDirectories
     {
         static void Main(string[] args)
         {
+            // Files info
+            string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+            FileInfo info = new FileInfo(fileName);
+            System.Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extensions: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+
+            // Determining file name extensions
+            System.Console.WriteLine(Path.GetExtension("sales.json"));
+
+            // Combining paths
+            System.Console.WriteLine(Path.Combine("stores", "201"));
+
+            // Use current symbol seporator for path
+            System.Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+
+            // Get special folder lie MyDocuments for Windows, macOS or Linux
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            System.Console.WriteLine($"Documents path: {docPath}");
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            // Get current diryctory:
+            System.Console.WriteLine($"Current dirctory: {currentDirectory}");
+        }
+
+        static void DirectoryDemo(){
             var salesFiles = FindFiles("stores");
 
             foreach(var file in salesFiles)
